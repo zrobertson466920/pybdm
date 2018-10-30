f5d9cc3d318c04e11ff6b1644a3439a877d0c943
zrobertson <zachary_w_robertson@hotmail.com>
2018-10-29 20:38:05 -0500

    Switched to Numpy Arrays

--- BDM.py
+++ BDM.py
@@ -7,33 +7,39 @@ Created on Thu Oct 25 00:21:24 2018
 """
 
 import numpy as np
-from collections import Counter
+import ast
+import json
 
+from collections import Counter
+from itertools import product
+
+
+# The JSON holds tuples
 def build_lookup_table(filename):
-    with open(filename,'r') as datafile:
-        lookup = {}
-        for line in datafile.readlines():
-            items = line.split(',')
-            base_matrix = items[0]
-            kvalue = float(items[1])
-            lookup[base_matrix] = kvalue
+    with open(filename, 'r') as fp:
+        lookup = json.load(fp)
+
     return lookup
 
 
-def string_to_nestedlist(string):
-    return [[int(s) for s in row] for row in string.split('-')]
+def key_to_array(string):
+    return np.array(ast.literal_eval(string))
 
 
-def nestedlist_to_string(nestedlist):
-    return '-'.join([''.join([str(i) for i in row]) for row in nestedlist])
+# Needs to be general
+def array_to_key(matrix):
+    try:
+        return str(tuple(tuple(i) for i in matrix))
+    except TypeError:
+        return str(matrix)
 
 
-def calculate_bdm(string, lookup, d=4, boundaries='ignore', verbose=False):
+def calculate_bdm(array, lookup, block=4, dim=2, boundaries='ignore', verbose=False):
     """
     Input
     --------
     string : string
-        binary matrix in string format, where rows are separated by minus signs
+        numpy array
     lookup : dict
         key is base matrix, value is its CTM value. Base matrices must be of
         size d x d.
@@ -45,37 +51,38 @@ def calculate_bdm(string, lookup, d=4, boundaries='ignore', verbose=False):
     bdm : float
         BDM value for the matrix
     """
+
+    shape = np.shape(array)
+    dim = len(shape)
+
     assert boundaries == 'ignore', 'Boundary conditions not implemented yet.'
-    
-    rows = string_to_nestedlist(string)
+    assert shape.count(shape[0]) == len(shape), 'Only square matrices are allowed.'
+    assert dim == 2, 'Only matrices are allowed'
+
     if verbose:
-        print('The full matrix to be decomposed:')
-        print(np.array(rows))
-    nrows, ncols = len(rows), len(rows[0])
-    
-    for row in rows:
-        assert len(row) == ncols, '{}'.format()
-    
-    n_rowblocks = int(nrows / d)
-    n_colblocks = int(ncols / d)
-    
+        print('The full matrix (%i,%i) to be decomposed:' % shape)
+        print(array)
+
+    # Alter shape for iteration
+    shape = np.array(shape) // block
+    func = lambda x: range(0,x,1)
+    shape = map(func,shape)
+
     # Leftovers will be ignored
-        
     submatrices = []
-    for i in range(n_rowblocks):
-        for j in range(n_colblocks):
-            # pick a submatrix in list-of-lists format
-            submatrix = [row[d * j : d * (j+1)] for row in rows[d * i : d * (i+1)]]                
-            # convert to string format
-            string = nestedlist_to_string(submatrix)
-            submatrices.append(string)
+    for item in product(*list(shape)):
+        sm = array
+        for i in range(0,dim):
+            sm = np.take(sm,list(range(item[i]*block,(item[i]+1)*block)),axis = i)
+        submatrices.append(array_to_key(sm))
+
     counts = Counter(submatrices)
     bdm_value = sum(lookup[string] + np.log2(n) for string, n in counts.items())
     
     if verbose:
         print('Base submatrices:')
         for s, n in counts.items():
-            submatrix = string_to_nestedlist(s)
+            submatrix = key_to_array(s)
             print(np.array(submatrix), 'n =', n)
         print('BDM calculated value =', bdm_value)
     
@@ -87,14 +94,14 @@ def calculate_bdm(string, lookup, d=4, boundaries='ignore', verbose=False):
 
 if __name__ == '__main__':
 
-    lookup = build_lookup_table('D5.CSV')
+    lookup = build_lookup_table('K-4x4.json')
     strings = []
     with open('example_input.txt','r') as input_file:
         for line in input_file.readlines():
             # Removes all kinds of whitespace and can handle edge case where file doesn't terminate with newline char
             strings.append(line.rstrip())
 
-    bdmvals = [calculate_bdm(s, lookup, verbose=True) for s in strings]
+    bdmvals = [calculate_bdm(key_to_array(s), lookup, verbose=True) for s in strings]
     for val in bdmvals:
         # I get the same vals as with the original script
         print(val)
