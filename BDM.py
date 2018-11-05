407062359cc3eabe0516ad628687470ce4b68701
zrobertson <zachary_w_robertson@hotmail.com>
2018-11-05 12:13:20 -0600

    Added entropy function to calculate BDM

--- BDM.py
+++ BDM.py
@@ -88,14 +88,17 @@ def calculate_bdm(array, lookup, block=4, dim=2, boundaries='ignore', verbose=Fa
 
     submatrices = partition(array, block, dim, boundaries, verbose)
     counts = Counter(submatrices.flatten())
-    bdm_value = sum(lookup[string]+np.log2(n) for string, n in counts.items())
-    
+    bdm_value = sum(lookup[string]+n*np.log2(n) for string, n in counts.items())
+    a = len(array[array == 0])
+    b = len(array[array == 1])
+    #bdm_value = 25+-(a * np.log(a/(a+b)+0.01)+b*np.log(b/(a+b)+0.01))
+
     if verbose:
         print('Base submatrices:')
         for s, n in counts.items():
             print(key_to_array(s), 'n =', n)
         print('BDM calculated value =', bdm_value)
-    
+
     # Just to check whether there were repetitions in the decomposition
     # print('Were all submatrices unique?', set(counts.values()) == {1})
 
@@ -105,10 +108,15 @@ def calculate_bdm(array, lookup, block=4, dim=2, boundaries='ignore', verbose=Fa
 def calculate_rbdm(array, lookup, block=3, dim=2, boundaries='ignore', verbose=False):
 
     submatrices = partition(array, block, dim, boundaries, verbose)
-    renorm_matrix = np.zeros(np.shape(submatrices))
+    renorm_matrix = np.zeros(np.shape(submatrices), dtype = int)
     for index, value in np.ndenumerate(submatrices):
         renorm_matrix[index] = flatten(key_to_array(value))
-    return calculate_bdm(array, lookup, 3, dim, boundaries, verbose)
+    return calculate_bdm(renorm_matrix, lookup, len(array) // block, dim, boundaries, verbose)
+
+
+def bit(string):
+    temp = key_to_array(string)
+    return(len(temp[temp == 0]))
 
 
 def flatten(square):
@@ -138,11 +146,11 @@ def flips(lookup1):
 
 if __name__ == '__main__':
 
-    lookup_base = build_lookup_table('K-3.json')
+    lookup_base = build_lookup_table('K-6.json')
     lookup_12 = build_lookup_table('K-12m.json')
     strings = list(lookup_12)
 
-    values = [(calculate_rbdm(key_to_array(s), lookup_base, block = 4, dim = 1, verbose=False),lookup_12[s]) for s in strings]
+    values = [(calculate_rbdm(key_to_array(s), lookup_base, block = 2, dim = 1, verbose=False),lookup_12[s]) for s in strings]
     bdm_vals,ctm_vals = zip(*values)
 
     pvalues = []
@@ -150,15 +158,17 @@ if __name__ == '__main__':
     bdm2 = []
     for s in strings:
         temp = key_to_array(s)
+        bdm1.append(len(temp[temp == 0]))
         p_mean = []
         pp_mean = []
         for i in range(0,12):
             p_val = flip(key_to_array(s))
-            p_mean.append(calculate_rbdm(p_val, lookup_base, block = 4, dim = 1, verbose=False))
+            p_mean.append(calculate_rbdm(p_val, lookup_base, block = 2, dim = 1, verbose=False))
             pp_mean.append(lookup_12[str(array_to_tuple(p_val))])
         pvalues.append((np.mean(p_mean),np.mean(pp_mean)))
     p_bdm, p_ctm = zip(*pvalues)
 
+    bdm1 = np.array(bdm1)
     bdm_vals = np.array(bdm_vals)
     p_bdm = np.array(p_bdm)
     ctm_vals = np.array(ctm_vals)
@@ -170,9 +180,12 @@ if __name__ == '__main__':
     print(len(score[score > 0]) / len(score))
 
     plt.figure()
-    print(scipy.stats.pearsonr(d_bdm,d_ctm))
-    #plt.scatter(ctm_vals,d_ctm, c = 'r')
-    plt.scatter(bdm_vals,d_bdm, c = 'b')
+    print(scipy.stats.pearsonr(bdm_vals,ctm_vals))
+    plt.scatter(bdm_vals,ctm_vals, c = 'r')
+    '''for n in range(1,12):
+        temp = abs(ctm_vals - bdm_vals)**2
+        plt.scatter(n,np.mean(temp[np.where(bdm1 == n)]), c = 'b')
+    plt.scatter(bdm1, 2.19*np.exp(-(6-bdm1)**2 / 12),c='r')'''
     plt.xlabel("BDM Value")
     plt.ylabel("CTM Value")
     plt.title("BDM vs. CTM")
